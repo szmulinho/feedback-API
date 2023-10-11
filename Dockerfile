2c609b0ca497ffbb2625e65b72463ba5103febde
szmulinho <samik1997@gmail.com>
2023-10-11 14:11:07 +0200

    model update

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,17 @@
 FROM golang:1.21.1-alpine AS build
 
-WORKDIR /app
+WORKDIR /feedback
 COPY . .
 
 RUN apk add --no-cache git
 RUN go mod download
-RUN CGO_ENABLED=0 GOOS=linux go build -o app
+RUN CGO_ENABLED=0 GOOS=linux go build -o feedback
 
 FROM alpine:latest
 
-WORKDIR /app
-COPY --from=build /app/app /app/app
+WORKDIR /feedback
+COPY --from=build /feedback/feedback /feedback/feedback
 
-EXPOSE 8091
+EXPOSE 8093
 
-CMD ["./app"]
+CMD ["./feedback"]
--- internal/model/model.go
+++ internal/model/model.go
@@ -1,24 +1,6 @@
 package model
 
-import (
-	"os"
-)
-
 var Feed []Opinion
-
-var JwtKey = []byte(os.Getenv("JWT_KEY"))
-
-type JwtToken struct {
-	Token string `json:"token"`
-}
-
-type Exception struct {
-	Message string `json:"message"`
-}
-
-type Response struct {
-	Data string `json:"data"`
-}
 
 type Opinion struct {
 	ID      int64  `json:"id" gorm:"primaryKey;autoIncrement"`
@@ -27,9 +9,3 @@ type Opinion struct {
 }
 
 var Feedback Opinion
-
-type JwtUser struct {
-	Jwt      string "jwt"
-	Password string "password"
-	Role     string `json:"role"`
-}
