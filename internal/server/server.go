75af5a004d47c4b4a93b848fa6566360d019c2eb
szmulinho <samik1997@gmail.com>
2023-10-20 14:32:43 +0200

    server

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"context"
-	"fmt"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/szmulinho/feedback/internal/server/endpoints"
@@ -26,8 +25,7 @@ func Run(ctx context.Context, db *gorm.DB) {
 		handlers.MaxAge(86400),
 	)
 	go func() {
-		err := http.ListenAndServe(fmt.Sprintf(":%s", "https://szmul-med-feedback.onrender.com/"), cors(router))
-
+		err := http.ListenAndServe(":8083", cors(router))
 		if err != nil {
 			log.Fatal(err)
 		}
