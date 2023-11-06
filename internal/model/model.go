7ad2147847ccf256ac165c087633fe22eea7e029
szmulinho <samik1997@gmail.com>
2023-11-06 19:11:25 +0100

    model

--- internal/model/model.go
+++ internal/model/model.go
@@ -4,6 +4,7 @@ var Feed []Opinion
 
 type Opinion struct {
 	ID      int64  `json:"id" gorm:"primaryKey;autoIncrement"`
+	Login   string `json:"login"`
 	Rating  int    `json:"rating" gorm:"column:rating"`
 	Comment string `json:"comment"`
 }
